eecd3c402ba619564547e9f28ad5f413e81a4b95
eel3 <eel3@users.noreply.github.com>
2014-07-08 23:22:15 +0900

    Add T4 (Text Template Transformation Toolkit) sample (experimental)

---
+++ t4/README.md
@@ -0,0 +1,14 @@
+args (sample code)
+==================
+
+args T4 (Text Template Transformation Toolkit) implementation.
+
+Target environments
+-------------------
+
+Windows + Visual Studio 2010 (need TextTransform.exe).
+
+Usage
+-----
+
+Call args.bat.
---
+++ t4/args.ps1
@@ -0,0 +1,20 @@
+Set-StrictMode -Version Latest
+
+Set-Variable -Name T4 -Value 'C:\Program Files (x86)\Common Files\Microsoft Shared\TextTemplating\10.0\TextTransform.exe' -option Constant
+
+if ($args.length -eq 0) {
+    exit
+}
+$argv = '-a', "!!np!$($args.length)"
+for ($i = 0; $i -lt $args.length; $i++) {
+    $argv += '-a'
+    $argv += "!!p$i!$($args[$i])"
+}
+
+$output = [System.IO.Path]::GetTempFileName()
+$scriptRoot = Split-Path -Path $MyInvocation.MyCommand.Path
+
+. "$T4" '-o' "$output" $argv "$scriptRoot\args.tt"
+
+Get-Content $output
+Remove-Item $output
